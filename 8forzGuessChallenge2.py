d1eaee8df439801112d77b61612a82902f32b897
juicyPINEAPPLE421 <yxhuang14@gmail.com>
2024-07-26 11:59:06 -0300

    extra practice

---
+++ 8forzGuessChallenge2.py
@@ -0,0 +1,15 @@
+#use a while loop to get inputs of grocery items and put them in a list
+#while loop stops when users enters 0
+#use for loop to output the grocery items in a full sentence
+
+#ex.
+#Input:
+#Apples
+#Oranges
+#Oreos
+#0
+
+#Output
+#You bought Apples
+#You bought Oranges
+#You bought Oreos
